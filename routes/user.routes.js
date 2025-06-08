75a54a7b3d3b30965654f2deeb8b76bb8eed79ea
Ruksana khatun <iitinaruksana@gmail.com>
2025-06-08 19:04:38 +0530

    require userMiddleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose');
-const User = require('../models/user');
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const User = require('../models/User.js');
+const userMiddleware = require('../middleware/userMiddleware.js'); // ✅ Correct
+
+const { registerUser, loginUser } = require('../controllers/user.controller.js');
+
+// Routes
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+
+module.exports = router;
 
 
-// User registration route
-router.post("/register",registerUser);
-router.post("/login",loginUser);
