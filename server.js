285a1950946814bc98454572661fe9c757466255
Ruksana khatun <iitinaruksana@gmail.com>
2025-06-08 19:05:29 +0530

    FIX Router code

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express=require('express');
 const morgan = require('morgan');
 const app=express();
 const path=require('path');
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
 const PORT=3000;
+const userMiddleware = require('./middleware/userMiddleware')
+const userRoutes = require('./routes/user.routes');
+
 // middleware
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.json());
@@ -10,19 +16,22 @@ app.use(express.urlencoded({extended:true}));
 app.use((morgan('dev')));
 
 // routes
+app.use("/api/user",userRoutes);
+// app.use('/chatbot',chatbot);
 
 
 app.get('/ruksana',(req,es)=>{
     es.send("hi hello how ae you")
 });
-app.get("/api",(req,res)=>{
-    res.json({
-        name:"Ruksana",
-        age:25,
-        city:"Delhi"
-    })
-});
-app.listen(PORT,(req,res)=>{
-    console.log(`Server is running on port ${PORT}`);
 
+mongoose.connect(process.env.MONGO_URL|| 'mongodb://localhost:27017/mydatabase')
+.then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
 })
+.catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+});
+
