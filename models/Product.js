b080d40d300a455a5a2caeba9ab7a1bded0ad1f8
Ruksana khatun <iitinaruksana@gmail.com>
2025-06-08 19:07:12 +0530

    added product modles

---
+++ models/Product.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const productSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  price: Number,
+  imageUrl: String
+});
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;;
